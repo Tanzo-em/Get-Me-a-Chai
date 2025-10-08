c49b7901f4445f3dbc63a5caddd6202d4921d871
Rakesh <rakesh.rkt121@gmail.com>
2025-10-08 15:29:30 +0530

    name of dbchange

--- db/connectDb.js
+++ db/connectDb.js
@@ -15,7 +15,7 @@ if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-async function dbConnect() {
+async function connectDb() {
   if (cached.conn) {
     return cached.conn;
   }
@@ -27,4 +27,4 @@ async function dbConnect() {
   return cached.conn;
 }
 
-export default dbConnect;
+export default connectDb;
